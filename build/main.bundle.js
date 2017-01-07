413a69c251952210db6c3b95f3142a099cae5afb
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-01-07 12:43:20 -0700

    functioning test suite

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -84,7 +84,7 @@
 
 
 	// module
-	exports.push([module.id, "body {\n  background: #cec2c2;\n  font-family: helvetica, sans-serif;\n  font-weight: 100;\n  letter-spacing: 2px;\n  text-transform: uppercase; }\n\nh3 {\n  position: relative;\n  top: 50px;\n  font-weight: 100;\n  font-size: 30px;\n  color: #5a5959;\n  text-align: center; }\n\nbutton {\n  padding: 20px;\n  border-radius: 2px;\n  outline: none;\n  border: none;\n  background: #982292;\n  color: white;\n  margin: 10px;\n  width: 100px;\n  font-family: helvetica, sans-serif;\n  font-weight: 100;\n  letter-spacing: 2px;\n  text-transform: uppercase; }\n  button:hover {\n    background: #450f42;\n    cursor: pointer; }\n\n.LikesContainer {\n  height: 300px;\n  width: 300px;\n  margin: 78px auto;\n  background: rgba(255, 255, 255, 0.4); }\n\n.ActionButtons {\n  width: 240px;\n  margin: 100px auto 0 auto; }\n", ""]);
+	exports.push([module.id, "body {\n  background: #ff009f;\n  font-family: helvetica, sans-serif;\n  font-weight: 100;\n  letter-spacing: 2px;\n  text-transform: uppercase; }\n\nh3 {\n  position: relative;\n  top: 50px;\n  font-weight: 100;\n  font-size: 30px;\n  color: #5a5959;\n  text-align: center; }\n\nbutton {\n  padding: 20px;\n  border-radius: 2px;\n  outline: none;\n  border: none;\n  background: #982292;\n  color: white;\n  margin: 10px;\n  width: 100px;\n  font-family: helvetica, sans-serif;\n  font-weight: 100;\n  letter-spacing: 2px;\n  text-transform: uppercase; }\n  button:hover {\n    background: #450f42;\n    cursor: pointer; }\n\n.LikesContainer {\n  height: 300px;\n  width: 300px;\n  margin: 78px auto;\n  background: rgba(255, 255, 255, 0.4); }\n\n.ActionButtons {\n  width: 240px;\n  margin: 100px auto 0 auto; }\n", ""]);
 
 	// exports
 
